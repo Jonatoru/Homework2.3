0581b8eade72d3f6fa81c57e68ff7f3f5d25b8cb
Jonatoru <jetrence95@gmail.com>
2022-11-08 00:41:18 +0500

    Инициализированы вес боксеров, произвл сложение и вычитание их веса.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        var boxerWeightOne = 78.2;
+        System.out.println("Вес первого боксера " + boxerWeightOne + "кг.");
+        var boxerWeightTwo = 82.7;
+        System.out.println("Вес второго боксера " + boxerWeightTwo + "кг.");
+        var TotalWeight = boxerWeightOne + boxerWeightTwo;
+        System.out.println("Общий вес двух боксеров " + TotalWeight + "кг.");
+        var WeightDifference = boxerWeightTwo - boxerWeightOne;
+        System.out.println("Разница в весе составляет " + WeightDifference + "кг.");
+    }
+}
